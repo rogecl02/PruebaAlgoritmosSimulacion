fca71e5c6e4bb407f379e70db9dbf7be4db30e26
roge cl <rogecl02@gmail.com>
2024-10-07 17:51:44 -0600

    hola

---
+++ Algoritmos/GeneradorAleatorios.cs
@@ -0,0 +1,24 @@
+﻿using PruebaAlgoritmosSimulacion.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaAlgoritmosSimulacion.Algoritmos
+{
+    public class GeneradorAleatorios
+    {
+        public List<Asignacion> CrearListaOrigen(int puntosTotales,int limiteInferior, int limiteSuperior)
+        { 
+            List<Asignacion> listaAsignacion = new List<Asignacion>();
+            for (int i =0; i < puntosTotales; i++)
+            {
+                Asignacion generador = new Asignacion();
+                generador.Latitud = Random(0,100);
+            }
+            return listaAsignacion;
+        }
+
+    }
+}
