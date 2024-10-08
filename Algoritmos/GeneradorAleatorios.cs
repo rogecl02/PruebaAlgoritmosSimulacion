f023750378e454bcdc396b6f4494ae94b3883af1
roge cl <rogecl02@gmail.com>
2024-10-07 18:07:10 -0600

    hola1.2

--- Algoritmos/GeneradorAleatorios.cs
+++ Algoritmos/GeneradorAleatorios.cs
@@ -14,8 +14,15 @@ namespace PruebaAlgoritmosSimulacion.Algoritmos
             List<Asignacion> listaAsignacion = new List<Asignacion>();
             for (int i =0; i < puntosTotales; i++)
             {
+                Random aleatorio =new Random();
                 Asignacion generador = new Asignacion();
-                generador.Latitud = Random(0,100);
+               
+                generador.Latitud = aleatorio.Next(limiteSuperior);
+                generador.Longitud = aleatorio.Next(limiteSuperior);
+                generador.IdPunto = i;
+                generador.Especie = i.ToString();
+                generador.Activo = false;
+                listaAsignacion.Add(generador);
             }
             return listaAsignacion;
         }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace PruebaAlgoritmosSimulacion
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
